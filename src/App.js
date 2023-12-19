2cef91291b9fa9db006aebd0c4524afd699d6bce
Jeee7 <arya.ziddan7@gmail.com>
2023-12-20 06:59:19 +0700

    uppping

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import AppNavbar from './Components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MiddleComponent from './Components/MiddleComponent';
 import HighlightImage from './Components/HighlightImage';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+      <title>D Fast Rent Car</title>
+        <meta name="description" content="Rental murah plus driver di tanggerang selatan" />
+      </Helmet>
       <AppNavbar />
       <HighlightImage />
       <MiddleComponent />
